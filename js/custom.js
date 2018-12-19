be932344467dfbf042c931c8990f803d1a5ec368
mark johnson <mjohnson24@comcast.net>
2018-12-19 12:16:02 -0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+/* TWEEEN MAX */
+var overlay = document.getElementById('overlay');
+
+// TweenMax.to(overlay, 2, {
+//     delay: 1,
+//     top: "-100%",
+//     ease: Expo.easeInOut
+// })
+
+// TweenMax.to("overlay span", 2, {
+//     opacity: 0,
+//     y: -60,
+//     ease: Expo.easeInOut
+// })
